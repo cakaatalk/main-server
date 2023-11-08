37c5c127a83210cec56b5e3e0d3783c667f33b21
donghyun <ilovekdh1208@ajou.ac.kr>
2023-11-08 23:07:18 +0900

    docs : 토큰을 DB에 저장해야하는 이유 추가

--- src/common/jwt/jwt.controller.js
+++ src/common/jwt/jwt.controller.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken')
-const SECRET_KEY = process.env.JWT_SECRET;
+const AuthToken = require('../../modules/auth/AuthToekn.js')
+const ACCESS_SECRET_KEY = process.env.ACCESS_TOKEN_PRIVATE_KEY;
+const REFRESH_SECRET_KEY = process.env.REFRESH_TOKEN_PRIVTATE_KEY;
 
 exports.generateAccessToken = (email) => {
     return jwt.sign(
@@ -8,7 +10,7 @@ exports.generateAccessToken = (email) => {
             time: Date(),
             email: email
         },
-        secret = SECRET_KEY,
+        secret = ACCESS_SECRET_KEY,
         options = {
             expiresIn: '15m'
         }
@@ -16,8 +18,32 @@ exports.generateAccessToken = (email) => {
 }
 
 exports.generateRefreshToken = (email) => {
-
+    return jwt.sign(
+        payload = {
+            type: 'JWT',
+            time: Date(),
+            email: email
+        },
+        secret = REFRESH_SECRET_KEY,
+        options = {
+            expiresIn: '30d'
+        }
+    )
 }
+
+exports.generateTokens = async (email) => {
+    try {
+
+
+        const userToken = await UserToken.findOne({ userId: user._id });
+        if (userToken) await userToken.remove();
+
+        await new UserToken({ userId: user._id, token: refreshToken }).save();
+        return Promise.resolve({ accessToken, refreshToken });
+    } catch (err) {
+        return Promise.reject(err);
+    }
+};
 
 // TODO: DB에 AccessToken을 저장해 관리
 exports.destroyAccessToken = (req, res, next) => {
