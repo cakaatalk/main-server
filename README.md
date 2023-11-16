37e399e43020cfb8c808c4e4dd125a1dc3f1313e
donggni0712 <donggni0712@naver.com>
2023-11-16 12:57:15 +0900

    feat: Refactor

--- README.md
+++ README.md
@@ -28,5 +28,5 @@ app.js
 ```
 
 ```
-docker run --name mysql-cakaotalk -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_DATABASE=cakaotalk -e MYSQL_ROOT_HOST=% -p 4040:3306 -d mysql
+docker run --name mysql-cakaatalk -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_DATABASE=cakaatalk -e MYSQL_ROOT_HOST=% -p 4040:3306 -d mysql
 ```
--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,52 +1,51 @@
-const jwtController = require('../../common/jwt/jwt.controller');
-const userService = require('../user/user.service');
+const jwtController = require("../../common/jwt/jwt.controller");
+const userService = require("../user/user.service");
 
-exports.signUpAndGiveToken = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+// exports.signUpAndGiveToken = async (req, res) => {
+//     try {
+//         const { email, password } = req.body;
 
-    }
-}
+//     }
+// }
 
 exports.loginAndGiveToken = async (req, res) => {
-    try {
-        const email = req.query.email;
-        const result = await userService.findUserByEmail(email);
-        const accessToken = jwtController.generateAccessToken(result[0].email);
-        res.status(200).json({ accessToken: accessToken });
-    } catch (error) {
-        console.error('Error occurred while finding user by email:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-}
-
-// TODO: 다른 사람이 로그인 했을 때 강제 로그아웃
-exports.forceLogout = () => { }
-
-exports.logoutAndDestroyToken = async (req, res) => {
+  try {
     const email = req.query.email;
     const result = await userService.findUserByEmail(email);
-    jwtController.destroyAccessToken(req, res);
-    res.status(200).json({ message: "Successfuly logout" });
-}
+    const accessToken = jwtController.generateAccessToken(result[0].email);
+    res.status(200).json({ accessToken: accessToken });
+  } catch (error) {
+    console.error("Error occurred while finding user by email:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// TODO: 다른 사람이 로그인 했을 때 강제 로그아웃
+exports.forceLogout = () => {};
+
+exports.logoutAndDestroyToken = async (req, res) => {
+  const email = req.query.email;
+  const result = await userService.findUserByEmail(email);
+  jwtController.destroyAccessToken(req, res);
+  res.status(200).json({ message: "Successfuly logout" });
+};
 
 // TODO: next 넣어서 미들웨어로 만들기
 exports.checkUserSession = async (req, res) => {
-    try {
-        const accessToken = req.headers.authorization;
-        const refreshToken = extractRefreshTokenFromCookie(req);
-        const result = await jwtController.validateToken(accessToken, refreshToken);
-        console.log('Successfuly Authenticateed');
-        res.status(200).json({ email: result.email });
-    } catch (error) {
-        res.status(500).json({ error: error })
-    }
-}
+  try {
+    const accessToken = req.headers.authorization;
+    const refreshToken = extractRefreshTokenFromCookie(req);
+    const result = await jwtController.validateToken(accessToken, refreshToken);
+    console.log("Successfuly Authenticateed");
+    res.status(200).json({ email: result.email });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
 
 // Http Only Cookie에서 RefreshToken을 가져오는 함수
 function extractRefreshTokenFromCookie(req) {
-    // TODO: Http Only Cookie에서 RefreshToken을 가져오는 로직을 작성해주세요.
-    // 쿠키에서 RefreshToken을 추출하고 반환해야 합니다.
-    return req.cookies.refreshToken;
+  // TODO: Http Only Cookie에서 RefreshToken을 가져오는 로직을 작성해주세요.
+  // 쿠키에서 RefreshToken을 추출하고 반환해야 합니다.
+  return req.cookies.refreshToken;
 }
-
