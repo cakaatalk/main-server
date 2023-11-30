1c4713414eca3774fb20f3203eb353e1bb1bac7e
donggni0712 <donggni0712@naver.com>
2023-11-30 11:40:07 +0900

    feat(app): Fix path

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const app = express();
 const port = 8080;
 
 const routes = require("./src/route");
-const authRouter = require("./src/modules/auth/auth.routes");
+const { dongxeption } = require("#dongxeption");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api", routes);
-app.use((error, req, res, next) => {
-  res.status(500).json({ error: error.message });
-});
+
+app.use(dongxeption);
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
