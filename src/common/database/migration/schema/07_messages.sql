f6d7349d55dcf78318a67b075946527532167018
donggni0712 <donggni0712@naver.com>
2023-12-02 21:32:38 +0900

    feat(chat): Make db for chat

---
+++ src/common/database/migration/schema/07_messages.sql
@@ -0,0 +1,9 @@
+CREATE TABLE MESSAGES (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL,
+    sender INT NOT NULL,
+    room_id INT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender) REFERENCES users(id),
+    FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
